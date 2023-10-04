ac7408a2b1bbe9f1b1c5b6ed10042b2da4997084
software-smith <work@rafaelfarcher.at>
2023-10-04 17:39:12 +0200

    chore: init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>HTML Example</title>
+  </head>
+  <body>
+    <w3m-button />
+    <script src="dist/bundle.js"></script>
+  </body>
+</html>
